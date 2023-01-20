51849001b737ae8b9a6b4ab3225b5a1a37383592
Kateryna Kleshch <ketrins1993@gmail.com>
2023-01-21 01:51:56 +0200

    add token cheking

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,4 +1,8 @@
 const { httpError } = require("../helpers");
+const jwt = require("jsonwebtoken");
+const { User } = require("../models/users");
+
+const { JWT_SECRET } = process.env;
 
 const validateRequestBody = (schema) => {
   return (req, res, next) => {
@@ -11,6 +15,34 @@ const validateRequestBody = (schema) => {
   };
 };
 
+const auth = async (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization || "";
+    console.log(authHeader);
+    const [type, token] = authHeader.split(" ");
+
+    if (type !== "Bearer" || !token) {
+      return next(httpError(401, "Not authorized"));
+    }
+    console.log(jwt.verify(token, JWT_SECRET));
+    const { id } = jwt.verify(token, JWT_SECRET);
+    const user = await User.findById(id);
+
+    req.user = user;
+  } catch (error) {
+    if (
+      error.name === "TokenExpiredError" ||
+      error.name === "JsonWebTokenError"
+    ) {
+      return next(httpError(401, "Not authorized"));
+    }
+    next(error);
+  }
+
+  next();
+};
+
 module.exports = {
   validateRequestBody,
+  auth,
 };
