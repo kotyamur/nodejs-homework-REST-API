4d08d88c15651146a4a4234336fe1549c2c6443c
Kateryna Kleshch <ketrins1993@gmail.com>
2023-01-20 22:06:54 +0200

    add user login

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,27 +1,25 @@
 const { User } = require("../models/users");
 const { httpError } = require("../helpers");
+const jwt = require("jsonwebtoken");
 
 const bcrypt = require("bcrypt");
+const { JWT_SECRET } = process.env;
 
 const signup = async (req, res, next) => {
-  const { email, password, subscription = "starter" } = req.body;
-  console.log(req.body);
-  const salt = await bcrypt.genSalt();
-  const hashedPassword = await bcrypt.hash(password, salt);
-
   try {
-    await User.create({
+    const { email, password } = req.body;
+    console.log(req.body);
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const savedUser = await User.create({
       email,
       password: hashedPassword,
-      subscription,
     });
 
     res.status(201).json({
-      data: {
-        user: {
-          email,
-          subscription,
-        },
+      user: {
+        email,
+        subscription: savedUser.subscription,
       },
     });
   } catch (error) {
@@ -32,6 +30,41 @@ const signup = async (req, res, next) => {
   }
 };
 
+const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    const storedUser = await User.findOne({
+      email,
+    });
+    console.log(storedUser);
+
+    if (!storedUser) {
+      return next(httpError(401, "email is not valid"));
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, storedUser.password);
+
+    if (!isPasswordValid) {
+      return next(httpError(401, "password is not valid"));
+    }
+
+    const payload = { id: storedUser._id };
+    const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "1h" });
+
+    return res.json({
+      token,
+      user: {
+        email,
+        subscription: storedUser.subscription,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = {
   signup,
+  login,
 };
--- routes/api/user.js
+++ routes/api/user.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const { signup } = require("../../controllers/userControllers");
+const { signup, login } = require("../../controllers/userControllers");
 const { validateRequestBody } = require("../../middlewares");
 const { userValidateSchema } = require("../../schemas/userSchema");
 
 const userRouter = express.Router();
 
 userRouter.post("/signup", validateRequestBody(userValidateSchema), signup);
+userRouter.post("/login", validateRequestBody(userValidateSchema), login);
 
 module.exports = {
   userRouter,
